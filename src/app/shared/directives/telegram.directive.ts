61d719414a01b3f63cac96866ec64014be6446a5
lehenshtein <lehenshtein@gmail.com>
2023-04-13 01:14:01 +0300

    fix(telegram directive): paste of tg nickname with @ symbol was allowed

--- src/app/shared/directives/telegram.directive.ts
+++ src/app/shared/directives/telegram.directive.ts
@@ -1,14 +1,15 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
+import { NgControl } from '@angular/forms';
 
 @Directive({
   selector: '[appTelegram]'
 })
 export class TelegramDirective {
   regexStr = '^[a-zA-Z0-9_]*$';
-  specialKeys: Array<string> = [ 'Backspace', 'Tab', 'End', 'Home' ];
+  specialKeys: Array<string> = [ 'Backspace', 'Tab', 'End', 'Home', 'Control' ];
 
-  constructor(private el: ElementRef) { }
-  @HostListener("keydown", ["$event"]) onKeydown(event: KeyboardEvent) {
+  constructor(private el: ElementRef, private control: NgControl) { }
+  @HostListener('keydown', ['$event']) onKeydown(event: KeyboardEvent) {
     if (this.specialKeys.indexOf(event.key) !== -1) {
       return;
     }
@@ -18,5 +19,11 @@ export class TelegramDirective {
     if (next && !String(next).match(this.regexStr)) {
       event.preventDefault();
     }
+  }
+  @HostListener('paste', ['$event']) onPaste(event: ClipboardEvent) {
+    event.preventDefault();
+    let paste = event.clipboardData?.getData('text');
+    let trimmed = paste?.replace(/[^a-zA-Z0-9_]/g, '');
+    this.control.control?.setValue(trimmed);
   }
 }
