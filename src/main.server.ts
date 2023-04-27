b57a24d72555d9058a9505ffa646faa037cfc16a
lehenshtein <lehenshtein@gmail.com>
2023-04-28 02:32:45 +0300

    feat(universal): main server changed for prod mode

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,2 +1,10 @@
+import { environment } from './environments/environment';
+import { enableProdMode } from '@angular/core';
+import '@angular/platform-server/init';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 export { AppServerModule } from './app/app.server.module';
+export { renderModule } from '@angular/platform-server';
