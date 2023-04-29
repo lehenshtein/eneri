d3a159157641aeb1e020a64f0edec8375133e8f1
lehenshtein <lehenshtein@gmail.com>
2023-04-30 02:01:18 +0300

    feat(universal): analytics

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ const apiUrl = 'https://eneri-be.herokuapp.com';
 export const environment = {
   production: true,
   apiUrl: apiUrl,
-  url: 'https://eneri.com.ua'
+  url: 'https://eneri.com.ua',
+  googleAnalyticsTrackingCode: 'G-VB7WZFKHTJ'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ const testApiUrl = 'http://localhost:3000';
 export const environment = {
   production: false,
   apiUrl: testApiUrl,
-  url: 'https://eneri.com.ua'
+  url: 'https://eneri.com.ua',
+  googleAnalyticsTrackingCode: 'fakeLocal'
 };
