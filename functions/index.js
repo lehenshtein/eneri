1bfed7791522f3fa9cc80771a735a09feeab00b5
lehenshtein <lehenshtein@gmail.com>
2023-05-01 02:58:46 +0300

    feat(universal): region changes for firebase

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
 const functions = require("firebase-functions");
 const universal = require(__dirname + "/dist/server/main").app();
 
-exports.ssr = functions.https.onRequest(universal);
+exports.ssr = functions.region('europe-west1').https.onRequest(universal);
